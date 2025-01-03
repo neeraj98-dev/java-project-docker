98c6e6feb204d86021110f7a050ce6bcf541b963
[Neeraj kumar] <[neerajraj985275@gmail.com]>
2025-01-03 11:00:15 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args){
+        Date currentDate = new Date();
+        System.out.println("Current date: " + currentDate);
+    }
+
+}
